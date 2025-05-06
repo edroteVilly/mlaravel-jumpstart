1bde2222d4f74e51f18ef91aa6e33ede177c575f
edroteVilly <edrote.villy@gmail.com>
2025-05-06 17:27:28 +0800

    Update greeting.blade.php

--- resources/views/greeting.blade.php
+++ resources/views/greeting.blade.php
@@ -5,7 +5,7 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="bg-gray-100 text-center p-10">
-    <img src="{{ asset('images/yourimage.jpg') }}" alt="Laravel Logo"
+    <img src="{{ asset('images/yourimage.jfif') }}" alt="Laravel Logo"
          class="mx-auto mt-4 w-16 h-16 object-contain">
     <h1 class="text-3xl text-blue-600 font-bold mt-4">
         Hello, {{ $name }}! Welcome to Laravel
